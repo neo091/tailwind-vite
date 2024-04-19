25e23728e99fd41c11f4edeb84b229114bd5e7de
neo091 <mneo666@gmail.com>
2024-04-19 12:33:47 +0200

    first commit

---
+++ vite-tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-2xl" >Hello World</h1>
+      <p>Proyecto echo por <a href="https://github.com/neo091" target="_blank" className=" text-indigo-800 underline " >neo091</a></p>
+
+      <div></div>
+    </>
+  )
+}
+
+export default App
